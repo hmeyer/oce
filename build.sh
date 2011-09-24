670800ec2f2b3cc4e64fc965900d456ed2684f8e
Henning Meyer <hmeyer@gmx.eu>
2011-09-24 20:24:13 +0200

    prefix and smaller fixes

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIST=natty git-buildpackage --git-debian-branch=hm/ubuntu --git-upstream-branch=master --git-ignore-new
+DIST=natty git-buildpackage -S --git-debian-branch=hm/ubuntu --git-upstream-branch=master --git-ignore-new
 
 rm -rf .pc
 rm -rf debian/patches
