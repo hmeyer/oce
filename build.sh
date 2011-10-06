ffab5235462339a808b0fced29387dbfb2ebd363
Henning Meyer <hmeyer@gmx.eu>
2011-10-06 19:45:25 +0200

    fixed cleaning

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 DIST=natty git-buildpackage -S --git-debian-branch=hm/ubuntu --git-upstream-branch=master --git-ignore-new
 #DIST=natty git-buildpackage --git-debian-branch=hm/ubuntu --git-upstream-branch=master --git-ignore-new
 
-rm -rf .pc
-rm -rf debian/patches
+dh clean
